c1c49e6183f8ee59f6509837e6b93cd1af9913b7
System Administrator <root@Elliotts-MacBook-Air.local>
2023-03-31 22:30:25 -0400

    basic UI

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
   
 app = Flask(__name__) #creating the Flask class object   
  
 @app.route('/') #decorator drfines the   
 def home():  
-    return "hello, this is our first flask website";  
+    return render_template('home.html')
   
 if __name__ =='__main__':  
     app.run(debug = True)
