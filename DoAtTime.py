d980c0c1f7877a431f17d1a0ad8e60bd7092853e
edwii-zhu <drawde37@hotmail.com>
2023-04-01 03:09:14 -0400

    actually main.py

--- DoAtTime.py
+++ DoAtTime.py
@@ -1,11 +1,16 @@
 import time
 from Randomtime import randomtime
 
-randomtime = randomtime()
+randt = randomtime()
+#show confirmation
 while True:
-    if time.time() > randomtime:
-        print("hi") #THIS IS WHERE YOU PUT THE CODEEE
-        break
-    else:
-        time.sleep(60)
+    while True:
+        if time.time() > randt:
+            #open text page
+            #wait until submit button is pressed
+            randt = randomtime() + 86400
+            break
+        else:
+            time.sleep(5)
 
+    #show confirmation
