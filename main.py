147ec413f721ff8622e794c8f294853dbd7e749f
Elliott Kobelansky <elliottkobelansky@Elliotts-Air.hitronhub.home>
2023-04-01 04:14:36 -0400

    move stuff around

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import *
   
 app = Flask(__name__) #creating the Flask class object   
  
-@app.route('/') #decorator drfines the   
+@app.route('/', methods=["GET", "POST"]) #decorator drfines the   
 def home():  
+    if request.method == 'POST':
+        print(request.form['text'])
+
     return render_template('home.html')
+
+@app.route('/history', methods=["GET", "POST"])
+def history():
+    if request.method == 'POST':
+        print(request.form['date'])
+    return render_template('history.html')
 
 @app.route('/friends')
 def friends():
     return render_template('friends.html')
-
-@app.route('/history')
-def history():
-    return render_template('history.html')
   
 if __name__ =='__main__':  
     app.run(port=8000, debug = True)
