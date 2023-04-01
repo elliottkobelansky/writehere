831f787385d84820a4ef92aaa589968ee1942728
edwii-zhu <drawde37@hotmail.com>
2023-04-01 00:37:29 -0400

    added functionality to write csvs

---
+++ msgappend.py
@@ -0,0 +1,13 @@
+import csv
+from datetime import *
+
+def msgappend (TS: int, msg: str):
+    dt = datetime.utcfromtimestamp(TS)
+    yr = dt.strftime("%Y")
+    mt = dt.strftime("%m")
+    dy = dt.strftime("%d")
+    with open("msgs.csv","a") as i:
+        writer = csv.writer(i)
+        writer.writerow((yr,mt,dy,msg))
+
+msgappend(1640323429,'HE said: "hi!"')
